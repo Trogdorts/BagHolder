01a2fa13960db38cae11c721f03a2a4b3461e08d
Criss <criss.ej.davis@gmail.com>
2025-10-22 20:28:08 -0500

    Add toggle for trade count badges

--- app/core/config.py
+++ app/core/config.py
@@ -13,6 +13,7 @@ DEFAULT_CONFIG = {
         "theme": "dark",
         "show_text": True,
         "show_unrealized": True,
+        "show_trade_count": True,
         "show_weekends": False,
         "highlight_weekends": True,
         "auto_dark_mode": True,
